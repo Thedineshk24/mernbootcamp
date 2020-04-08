aba896e9c06865882c2a84d1725a24f4a985937c
thedineshk24 <dineshkatariya244@gmail.com>
2020-04-09 03:16:56 +0530

    3rd commit

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,21 +1,32 @@
-
+const { check, validationResult } = require('express-validator');
 const User = require('../models/user');
 
 exports.signup = (req,res) => {
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error : errors.array() [0].msg,
+            location: errors.location,
+            param: "USER SIGNUP"
+        });
+    }
+
     const user = new User(req.body);
     user.save((err,user) => {
         if(err){
             return res.status(400).json({
                 err:"NOT able to save user in DB"
-            })
+            });
         }
         res.json({
             name: user.name,
             email:user.email,
             id:user._id
         });
-    })
-}
+    });
+};
 
 exports.signout = (req,res) => {
     res.json({
