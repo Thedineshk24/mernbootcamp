86cbf89f53acd8745d3f05394ac4a1d8b9cbce8c
thedineshk24 <dineshkatariya244@gmail.com>
2020-04-12 01:53:52 +0530

    6th commit

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -1,35 +1,33 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const {ObjectId} = mongoose.Schema;
- 
-const ProductCartSchema = new Schema({
-    product:{
-        type:ObjectId,
-        ref:"Product",
-    },
-    name:String,
-    count:Number,
-    price:Number,
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product"
+  },
+  name: String,
+  count: Number,
+  price: Number
 });
 
-const ProductCart = mongoose.model("ProductCart",ProductCartSchema);
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
 
-const OrderSchema = new Schema({
-    products:[ProductCartSchema],
-    transaction_id:{},
-    amount:{type:Number},
-    address:String,
-    updated:Date,
-    user:{
-        type:ObjectId,
-        ref:User,
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    updated: Date,
+    user: {
+      type: ObjectId,
+      ref: "User"
     }
-},{
-    timestamps:true,
-});
+  },
+  { timestamps: true }
+);
 
-const Order = mongoose.model("Order",OrderSchema);
+const Order = mongoose.model("Order", OrderSchema);
 
-module.exports = {
-    ProductCart,Order
-}
+module.exports = { Order, ProductCart };
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const {getUserById , getUser} = require('../controllers/user');
+const {getUserById , getUser, updateUser , userPurchaseList } = require('../controllers/user');
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 
 router.param("userId", getUserById);
 
+//get
+
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
+//update 
+
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
+// get 
+
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
+
+
 module.exports = router;
+
+
