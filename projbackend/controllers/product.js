afe39d2b257276b44911c4a35f13f1fee1a25cf8
thedineshk24 <dineshkatariya244@gmail.com>
2020-04-26 23:52:27 +0530

    11th commit

---
+++ projbackend/controllers/product.js
@@ -0,0 +1,61 @@
+const Product = require("../models/product");
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
+
+exports.getProductById = (req,res,next,id) => {
+    Product.findById(id)
+    .populate("category")
+    .exec((err,product) => {
+        if(err){
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+        req.product = product;
+        next(); 
+    });
+}
+
+
+
+exports.createProduct = (req,res) => {
+    let form = formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err,fields,file) => {
+        if(err){
+            return res.status(400).json({
+                error: "problem with a Image"
+            });
+        }
+
+        //TODO:  restritions in the fields
+        let product = new Product(fields);
+
+        // handle file here
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error: " Maximum File size is  3-MB "
+                });
+            }
+
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+
+        }
+
+        // save in DB
+
+        product.save((err,product) => {
+            if(err){
+                return res.status(400).json({
+                    error: "Saving t-shirt in DB Failed! "
+                });
+            }
+
+            res.json(product);
+        });
+    });
+}
