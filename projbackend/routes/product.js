404d6e098291a183f112c34a08e8c2a8d1bb0274
thedineshk24 <dineshkatariya244@gmail.com>
2020-05-31 20:19:02 +0530

    13th commit

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProductById,createProduct} = require('../controllers/product');
+const {getProductById,createProduct,getProduct,photo} = require('../controllers/product');
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 const {getUserById} = require('../controllers/user');
 
@@ -11,5 +11,9 @@ router.param("productId", getProductById);
 
 // all of the routes
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+
+
+router.get("/product/:productId",getProduct);
+router.get("/product/photo/:productId", photo);
 
 module.exports = router;
