425010224b0eb758b8526cdbd92b750f039fb22b
thedineshk24 <dineshkatariya244@gmail.com>
2020-07-09 17:58:34 +0530

    today work done !

---
+++ projfrontend/src/admin/ManageCategories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from '../core/Base';
+
+const ManageCategories = () => {
+    return (
+        <Base title="Manage Categories" description="Welcome to Manage-Category section">
+            <h1 className="text-white">My Assignment</h1>
+        </Base>
+    )
+}
+
+export default ManageCategories;
--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -16,6 +16,9 @@ const AdminDashBoard = () => {
                         <Link to="/admin/create/category" className="nav-link text-success">Create Category</Link>
                     </li>
                     <li className="list-group-item">
+                        <Link to="/admin/categories" className="nav-link text-success">Manage Category</Link>
+                    </li>
+                    <li className="list-group-item">
                         <Link to="/admin/create/product" className="nav-link text-success">Create Products</Link>
                     </li>
                     <li className="list-group-item">
